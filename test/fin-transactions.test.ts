388ba29ac7f049b2e6499a1348dea1f671c098d5
Sathish <sat.hariharan@gmail.com>
2022-05-19 09:06:11 +0530

    first commit

---
+++ test/fin-transactions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FinTransactions from '../lib/fin-transactions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fin-transactions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FinTransactions.FinTransactionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
