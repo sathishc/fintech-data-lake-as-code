5ad634b0a9c88c6597a7e31ed7e465957c383c9e
Sathish <sat.hariharan@gmail.com>
2022-05-25 14:39:57 +0530

    update stack with ingestion code

--- lib/fin-transactions-stack.ts
+++ lib/fin-transactions-stack.ts
@@ -1,11 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Asset } from 'aws-cdk-lib/aws-s3-assets';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as kinesis from 'aws-cdk-lib/aws-kinesis';
+import { aws_dms as dms } from 'aws-cdk-lib';
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as path from 'path';
 
 export class FinTransactionsStack extends Stack {
@@ -28,6 +30,16 @@ export class FinTransactionsStack extends Stack {
     const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(this, "secGroup", vpc.vpcDefaultSecurityGroup)
     securityGroup.addIngressRule(ec2.Peer.securityGroupId(securityGroup.securityGroupId),ec2.Port.tcp(3306),"MySQL Ingress")
 
+    // create a MySQL parameter group
+    const parameterGroup = new rds.ParameterGroup(this, 'MySQLAuroraParameterGroup', {
+      engine: rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_2_08_1 }),
+      parameters: {
+        binlog_format: 'ROW',
+        binlog_row_image: 'full',
+        binlog_rows_query_log_events: 'ON'
+      },
+    });
+
     const dbCluster = new rds.DatabaseCluster(this, 'Database', {
       engine: rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_2_08_1 }),
       credentials: rds.Credentials.fromGeneratedSecret(dbUserName),
@@ -39,9 +51,9 @@ export class FinTransactionsStack extends Stack {
         vpc,
         securityGroups:[securityGroup],
       },
+      parameterGroup:parameterGroup,
       s3ImportBuckets: [resourcesObject.bucket],
       defaultDatabaseName:"workshopDb",
-      // iamAuthentication:true
     });
 
     const proxy = new rds.DatabaseProxy(this, 'Proxy', {
@@ -52,7 +64,6 @@ export class FinTransactionsStack extends Stack {
       iamAuth:true
     });
 
-    const secretJson = dbCluster.secret?.secretValue.toJSON();
 
     // create a farget task to initiatize data and send test trasactions into the database
     // Create an ECS cluster
@@ -85,8 +96,84 @@ export class FinTransactionsStack extends Stack {
       }
     });
     
+    
+    // create a kinesis sgtream as target for the RDS data.
+    const stream = new kinesis.Stream(this, 'TransactionDataStream', {
+      streamName: 'fin-transactions-stream',
+    });
 
-    new cdk.CfnOutput(this, 'generateDataCommand', { value: `aws ecs run-task --cluster ${ecsCluster.clusterName} --capacity-provider-strategy capacityProvider=FARGATE,base=0,weight=1 --network-configuration "awsvpcConfiguration={subnets=[${vpc.publicSubnets[0].subnetId}],assignPublicIp='ENABLED'}" --task-definition ${generateDatataskDefinition.family}` })
+    // setup a role to access the Database secret from dms
+    const serviceAccessRole = new iam.Role(this,"dbSecretAccessRole",{
+      assumedBy: new iam.ServicePrincipal(`dms.${region}.amazonaws.com`),
+    });
+    dbCluster.secret?.grantRead(serviceAccessRole);
+    stream.grantReadWrite(serviceAccessRole);
+    
+    
+    // create the source endpoint for reading the Aurora Database
+    const sourceEndpoint = new dms.CfnEndpoint(this, 'AuroraSourceEndpoint', {
+      endpointType: 'source',
+      engineName: 'aurora',
+      // the properties below are optional
+      databaseName: 'workshopDb',
+      mySqlSettings: {
+          secretsManagerAccessRoleArn: serviceAccessRole.roleArn,
+          secretsManagerSecretId: dbCluster.secret!.secretArn,
+      },
+    });
+
+    // create the target endpoint for reading the Aurora Database
+    const targetEndpoint = new dms.CfnEndpoint(this, 'KinesisTargetEndpoint', {
+      endpointType: 'target',
+      engineName: 'kinesis',
+      // the properties below are optional
+      kinesisSettings: {
+        messageFormat: 'JSON',
+        noHexPrefix: false,
+        partitionIncludeSchemaTable: false,
+        serviceAccessRoleArn: serviceAccessRole.roleArn,
+        streamArn: stream.streamArn,
+      },
+    });
+
+    
+    // create the replication instance and task
+    const txnReplicationSubnetGroup = new dms.CfnReplicationSubnetGroup(this, 'TxnAnlyticsReplicationSubnetGroup', {
+      replicationSubnetGroupDescription: 'TxnAnlyticsReplicationSubnetGroup',
+      subnetIds: [
+        vpc.privateSubnets[0].subnetId,
+        vpc.privateSubnets[1].subnetId,
+        // vpc.publicSubnets[0].subnetId,
+        // vpc.publicSubnets[1].subnetId
+      ],
+    });
+    
+    const replicationInstance = new dms.CfnReplicationInstance(this, 'TransAnalyticsReplicationInstance', {
+      replicationInstanceClass: 'dms.c5.large',
+      replicationSubnetGroupIdentifier:txnReplicationSubnetGroup.ref,
+      vpcSecurityGroupIds:[securityGroup.securityGroupId]
+    });
+    
+    const replicationTask = new dms.CfnReplicationTask(this, 'TransAnalyticsReplicationTask', {
+      migrationType: 'full-load-and-cdc',
+      replicationInstanceArn: replicationInstance.ref, // "arn:aws:dms:ap-south-1:332009426877:rep:U3STAZJJO7SYJWN7YVVKR3ZPNPWZ5S74ZHEPSRA", //replicationInstance.ref,
+      sourceEndpointArn: sourceEndpoint.ref,
+      tableMappings: '{"rules": [{"rule-type": "selection","rule-id": "281805402","rule-name": "281805402","object-locator": {"schema-name": "workshopDb","table-name": "%"},"rule-action": "include","filters": []}]}',
+      targetEndpointArn: targetEndpoint.ref,
+    });
+
+    new cdk.CfnOutput(
+      this, 
+      'generateDataCommand', 
+      { value: `aws ecs run-task --cluster ${ecsCluster.clusterName} --capacity-provider-strategy capacityProvider=FARGATE,base=0,weight=1 --network-configuration "awsvpcConfiguration={subnets=[${vpc.publicSubnets[0].subnetId}],assignPublicIp='ENABLED'}" --task-definition ${generateDatataskDefinition.family}` }
+    );
+
+    new cdk.CfnOutput(
+      this, 
+      'startReplicationTask', 
+      { value: `aws dms start-replication-task --replication-task-arn ${replicationTask.ref} --start-replication-task-type start-replication` }
+    );
+    
 
   }
 }
